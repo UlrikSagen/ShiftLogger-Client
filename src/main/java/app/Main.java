e4b26e38c47fb7579b020ded2762449553783efe
Ulrik <ullsagen@hotmail.com>
2026-01-22 09:36:48 +0100

    Made some changes to make project compatible with Linux

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -13,9 +13,6 @@ public class Main {
     public static void main(String[] args) {
         FlatDarkLaf.setup();
         AppTheme.apply();
-        System.out.println("UI Button.background = " + UIManager.getColor("Button.background"));
-        System.out.println("UI Button.foreground = " + UIManager.getColor("Button.foreground"));
-        System.out.println("Current LAF = " + UIManager.getLookAndFeel().getName());
         SwingUtilities.invokeLater(() -> {
             Controller controller = new Controller(new TimeService(), new TimeRepository());
             new MainView(controller).showUI();
